e68e82313bffec16b8f3f9648e9514cfe242c4ff
Ad1tya-007 <adityakul0314@gmail.com>
2024-10-06 07:18:18 -0700

    added hosted link

--- src/utils/supabase/actions.ts
+++ src/utils/supabase/actions.ts
@@ -22,7 +22,7 @@ export async function signUpWithGithub() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
     options: {
-      redirectTo: 'http://localhost:3000/auth/callback',
+      redirectTo: 'https://jh-a-kim-app--3000.prod1.defang.dev/auth/callback',
       scopes: 'read:org user', // Add the necessary scopes here
     },
   });
